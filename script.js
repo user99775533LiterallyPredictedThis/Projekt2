c2ac07c49f0efbd41d91c7f2574548ca61f1bc58
user99775533LiterallyPredictedThis <uljana.jegorova@tptlive.ee>
2024-11-21 16:00:54 +0200

    ALMOSTDONE
    ITSALMOSTDONE

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ let output = document.getElementById('outputtext');
 
 let number = Math.floor(Math.random() * 100);
 
+let score = 0
+
+let userName = prompt("Please enter your name so that I can save your score:");
+
+fetch(url)
+
+
 btn.addEventListener('click', function () {
     checkAnswer()
 })
@@ -29,16 +36,18 @@ function checkAnswer() {
         output.innerHTML = `You guessed right, your number was ${number}!`
 
         number = Math.floor(Math.random() * 100);
-        
-       let score = score + 1
 
-        const message = { number: number  };
+        score = score + 1
+        const scoreEl = document.getElementById("score");
+        scoreEl.innerHTML = `<p>Score: ${score}</p`;
+
+        const message = { number: score, name: userName };
         fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(message),
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(message),
         });
 
 
@@ -56,9 +65,21 @@ async function fetchScore() {
     const data = await response.json();
     console.log(data)
 
-    const score = document.getElementById("score");
-    score.innerHTML = "<p>Score: 2</p>";
+    const found = data.reverse().find(item => userName == item.name);
 
+    console.log(found)
+
+    const score = document.getElementById("score");
+    score.innerHTML = "<p>Score: 0</p>";
+
+    // do something here
     
+    if (found) {
+        score.innerHTML = `Score: ${found.number}`;
+    }
+
+
 }
 fetchScore()
+
+console.log(number)
